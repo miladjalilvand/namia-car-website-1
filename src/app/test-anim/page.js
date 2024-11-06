5fdba36873799780e52ae9bbca3ee708495c9124
miladjalilvand <39867269+miladjalilvand@users.noreply.github.com>
2024-11-06 02:54:55 -0800

    fix menu drawer animation

--- src/app/test-anim/page.js
+++ src/app/test-anim/page.js
@@ -1,7 +1,11 @@
+import NavBarTE from "@/components/menu";
+import TestMenu from "@/components/menu";
 import Menu from "@/components/menuItem";
 
 export default function TestAnimation (){
     return (
-        <Menu/>
+       <div className="pt-10">
+         <NavBarTE/>
+       </div>
     );
 }
