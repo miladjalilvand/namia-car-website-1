785a69f5710b7fcd3cc81a3f6b56a4ecb073be18
miladjalilvand <39867269+miladjalilvand@users.noreply.github.com>
2024-11-05 22:30:51 -0800

    fix menu icon and navbar z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useRouter, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaFileContract, FaMoon, FaPersonBooth, FaPhone, FaSun } from "react-icons/fa";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 const themes = {
   light: {
@@ -17,12 +18,20 @@ const themes = {
 };
 
 const NavBar = () => {
+  const [isRotated, setIsRotated] = useState(false);
+
+
   const router = useRouter();
   const { theme, toggleTheme } = useTheme();
   const currentTheme = themes[theme];
   const [isOpen, changeNav] = useState(false);
   const currentPath = usePathname();
 
+  const handleClick = () => {
+    setIsRotated(!isRotated);
+    changeNav(!isOpen);
+
+};
   const pushin = (path) => {
     changeNav(false);
     router.push(`/${path}`);
@@ -39,15 +48,42 @@ const NavBar = () => {
   }, [currentTheme]);
 
   return (
-    <div>
+    <div className="flex flex-row z-50">
+                  <div className=" z-50">
+             <div 
+              className="flex flex-col  fixed space-y-px pt-4 cursor-pointer
+                self-center"
+              onClick={handleClick}> 
+                <motion.div
+                className="w-5 h-1 bg-black self-start"
+                animate={{ rotate: isRotated ? -45 : 0, y: isRotated ? 4 : 0, x: isRotated ? -6 : 0 }}
+                transition={{ duration: 1 }}
+                style={{ originX: 0, originY: 0 }}
+            ></motion.div>
+
+            <motion.div
+                animate={{ rotate: isRotated ? 45 : 0 }}
+                transition={{ duration: 1 }}
+                style={{ originX: 0.5, originY: 0 }}
+                className="w-10 h-1 bg-black"
+            ></motion.div>
+
+            <motion.div
+                animate={{ rotate: isRotated ? -45 : 0 }}
+                transition={{ duration: 1 }}
+                style={{ originX: 0.5, originY: 0.5 }}
+                className="w-5 h-1 bg-black self-end"
+            ></motion.div></div>
+
+
+              
+            </div>
       {isOpen ? (
-        <div className="flex">
-          <div className="fixed z-30 flex flex-col w-full bg-red-800 opacity-90 min-h-screen md:w-1/3">
+        <div className="flex ">
+          <div className="fixed z-40 flex flex-col w-full bg-red-800 opacity-90 min-h-screen md:w-1/3">
             <div className="flex flex-row items-start justify-between p-4">
               <div className="flex flex-row space-x-2">
-                <h1 onClick={() => changeNav((prev) => !prev)} className="cursor-pointer text-white">
-                  *Close
-                </h1>
+
                 <h1 onClick={() => pushin('/')} className="cursor-pointer text-white">
                   *Logo
                 </h1>
@@ -67,10 +103,11 @@ const NavBar = () => {
             </div>
             <div className="p-4 text-white">footer</div>
           </div>
-          <div onClick={() => changeNav(false)} className="fixed md:w-screen z-20 md:h-screen bg-black opacity-70"></div>
+          <div onClick={() => changeNav(false)} className="fixed md:w-screen z-30 md:h-screen bg-black opacity-70"></div>
         </div>
       ) : (
-        <div className="fixed border-b-2 border-black w-full" style={{ backgroundColor: currentTheme.background }}>
+        <div className="fixed border-b-2 z-40
+         border-black w-full" style={{ backgroundColor: currentTheme.background }}>
           <div dir="ltr" className="flex flex-row space-x-0 justify-between" style={{ backgroundColor: currentTheme.background }}>
             <div className="flex">
               <div className="w-8 h-8 md:h-12 md:w-12 bg-red-600 flex items-center justify-center cursor-pointer">
@@ -90,10 +127,7 @@ const NavBar = () => {
                 طرح تعویض
               </div>
             </div>
-            <div className="flex z-50">
-              <div>Logo</div>
-              <div className="cursor-pointer" onClick={() => changeNav((prev) => !prev)}>MENU</div>
-            </div>
+
           </div>
         </div>
       )}
--- src/components/menuItem.jsx
+++ src/components/menuItem.jsx
@@ -1,35 +1,39 @@
-"use client";
+
+"use client"
 import { useState } from "react";
 import { motion } from "framer-motion";
-
 const Menu = () => {
     const [isRotated, setIsRotated] = useState(false);
 
     const handleClick = () => {
         setIsRotated(!isRotated);
+ 
     };
 
     return (
-        <div onClick={handleClick} className="flex flex-col justify-center items-center pt-16 space-y-px cursor-pointer">
+        <div onClick={handleClick} className="w-10 flex flex-col space-y-px pt-16 cursor-pointer">
             <motion.div
-                animate={{ rotate: isRotated ? 45 : 0 }}
-                transition={{ duration: 0.5 }}
-                className="w-10 h-1 bg-black"
-                style={{originX: 0.5 ,originY:0.5 }}
+                className="w-5 h-1 bg-black self-start"
+                animate={{ rotate: isRotated ? -45 : 0, y: isRotated ? 4 : 0, x: isRotated ? -6 : 0 }}
+                transition={{ duration: 2 }}
+                style={{ originX: 0, originY: 0 }}
             ></motion.div>
 
             <motion.div
-                className={`w-10 h-1 bg-black ${isRotated ? 'hidden' : 'block'}`}
+                animate={{ rotate: isRotated ? 45 : 0 }}
+                transition={{ duration: 2 }}
+                style={{ originX: 0.5, originY: 0 }}
+                className="w-10 h-1 bg-black"
             ></motion.div>
 
             <motion.div
                 animate={{ rotate: isRotated ? -45 : 0 }}
-                transition={{ duration: 0.5 }}
-                className="w-10 h-1 bg-black"
-                style={{ originX: 0.5 ,originY:0.5 }}
+                transition={{ duration: 2 }}
+                style={{ originX: 0.5, originY: 0.5 }}
+                className="w-5 h-1 bg-black self-end"
             ></motion.div>
         </div>
     );
-}
+};
 
 export default Menu;
